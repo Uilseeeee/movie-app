0758818a4653afcddb9310aaa8d5b1edcc91c1cd
Uilsee <uilseeu437@gmail.com>
2025-01-29 23:53:10 +0800

    third

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
-import { useEffect } from "react";
+ 'use client'
+import {useEffect} from 'react';
+import axios from 'axios';
 
 
 export default function Home() {
 
-  const TMDB_BASE_URL = process.env.TMDB_BASE_URL
-  const TMDB_API_TOKEN = process.env.TMDB_API_TOKEN
+  
+  const TMDB_BASE_URL = process.env.TMDB_BASE_URL ;
+  const TMDB_API_TOKEN = process.env.TMDB_API_TOKEN ; 
   const getMovieData = async () => {
     try {
       console.log("this is runnnig")
-      ;
-      
-      const response = axios.get('${TMDB_BASE_URL}/movie/popular?language=en-US&page=1,' {
+        ;
+
+      const response = await axios.get(
+        '${ TMDB_BASE_URL }/movie/popular?language=en-US&page=1',{
         headers: {
           Authorization: 'Bearer ${TMDB_API_TOKEN}',
         },
@@ -22,11 +26,14 @@ export default function Home() {
       console.log(err)
     }
   };
+
+
+
   useEffect(() => {
     getMovieData();
   }, []);
 
-  )
+
 
   return (
     <div className="">
