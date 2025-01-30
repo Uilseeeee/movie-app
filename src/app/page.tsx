bcbf75865dcd01cff54dc0abd95d3d4d17a812a5
Uilsee <uilseeu437@gmail.com>
2025-01-30 11:57:47 +0800

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,45 @@
- 'use client'
-import {useEffect} from 'react';
-import axios from 'axios';
+"use client";
 
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
 
-  
-  const TMDB_BASE_URL = process.env.TMDB_BASE_URL ;
-  const TMDB_API_TOKEN = process.env.TMDB_API_TOKEN ; 
+const TMDB_BASE_URL = process.env.TMDB_BASE_URL;
+const TMDB_API_TOKEN = process.env.TMDB_API_TOKEN;
+const [loading, setLoading] = useState(false)
+const [error, setError] = useState(false)
+const [popularMoviesData, setPopularMoviesData] = useState([]);
+
+
   const getMovieData = async () => {
     try {
-      console.log("this is runnnig")
-        ;
-
+      console.log("this is runnnig", TMDB_BASE_URL);
+      setLoading(true); 
       const response = await axios.get(
-        '${ TMDB_BASE_URL }/movie/popular?language=en-US&page=1',{
-        headers: {
-          Authorization: 'Bearer ${TMDB_API_TOKEN}',
-        },
-      }
+        `${TMDB_BASE_URL}/movie/popular?language=en-US&page=1`,
+        {
+          headers: {
+            Authorization: `Bearer ${TMDB_API_TOKEN}`,
+          },
+        }
       );
-      console.log(response);
-
+      console.log(response.data.results);
+      
+      setLoading(false)
     } catch (err) {
-      console.log(err)
+      setLoading(false)
+      if(axios.isAxiosError(err)){
+        setError(err.response.data.status.message)
+      }
+     console.log(("this is the err", error));
+     
     }
   };
-
-
 
   useEffect(() => {
     getMovieData();
   }, []);
 
-
-
-  return (
-    <div className="">
-
-    </div>
-
-  );
+  return <div className=""></div>;
 }
